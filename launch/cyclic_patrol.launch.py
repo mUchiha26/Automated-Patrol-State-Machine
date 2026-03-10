eba200cd5e4dab267823d23f915ddea17f051731
mUchiha26 <RAHAL.Mohamed-Yassine@tek-up.de>
2026-03-11 00:21:32 +0100

    Global Project Structure

---
+++ launch/cyclic_patrol.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='autonomous_patrol_system',
+            executable='cyclic_patrol',
+            name='cyclic_patrol_node',
+            output='screen',
+            parameters=[
+                {'use_sim_time': True},
+                {'total_cycles': 3}
+            ]
+        )
+    ])
--- setup.py
+++ setup.py
@@ -4,24 +4,32 @@ package_name = 'autonomous_patrol_system'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.2.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # Required by ament_index
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        # Package manifest
         ('share/' + package_name, ['package.xml']),
+        
+        # ✅ Launch files
+        ('share/' + package_name + '/launch', [
+            'launch/cyclic_patrol.launch.py',
+        ]),
+        
+        # ✅ Config files 
+        ('share/' + package_name + '/config', [
+            'config/waypoints.yaml',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Yasseene',
     maintainer_email='RAHAL.Mohamed-Yassine@tek-up.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='Autonomous patrol system for TurtleBot4',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'cyclic_patrol = autonomous_patrol_system.cyclic_patrol_node:main',
